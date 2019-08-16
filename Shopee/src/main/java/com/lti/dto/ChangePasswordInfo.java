51c5aaf05734bb0324b160fc0a480fb43a4e065a
BitadruMegha <bitadrumaitrabm@gmail.com>
2019-08-16 12:13:16 -0700

    test change password

---
+++ Shopee/src/main/java/com/lti/dto/ChangePasswordInfo.java
@@ -0,0 +1,26 @@
+package com.lti.dto;
+
+public class ChangePasswordInfo {
+	private String password;
+	private String newpassword;
+
+	public ChangePasswordInfo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getNewpassword() {
+		return newpassword;
+	}
+
+	public void setNewpassword(String newpassword) {
+		this.newpassword = newpassword;
+	}
+}
