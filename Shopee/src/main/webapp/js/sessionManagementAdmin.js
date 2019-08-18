a976c542c680420656e35cc8fceaa8853df9a251
Shraddha Powar <scpowar@gmail.com>
2019-08-18 16:21:16 -0700

    admin check

--- Shopee/src/main/webapp/js/sessionManagementAdmin.js
+++ Shopee/src/main/webapp/js/sessionManagementAdmin.js
@@ -1,12 +1,11 @@
 $(function() {
 		$("#logoutAdmin").hide();
-		var adminEmail = sessionStorage.getItem("adminEmail");
+		var adminEmail = sessionStorage.setItem("adminEmail");
 		var adminId = sessionStorage.getItem("adminId");
 		
-		if (adminEmail != null) {
-			$("#userEmail").append(userEmail);
+		if (retailerEmail != null) {
+			$("#adminEmail").append(adminEmail);
 			$("#logoutAdmin").show();
-			$("#loginAdmin").hide();
-			$("#register").hide();
 		}
 	});
+
